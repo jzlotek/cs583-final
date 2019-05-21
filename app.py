34163a850aaf7073928cd00bb346359017a254e3
John Zlotek <jzlotek@gmail.com>
2019-05-21 18:06:24 -0400

    Req update

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask import send_from_directory
 
 app = flask.Flask(__name__)
 
-
 @app.route('/', methods=['GET'])
 def home():
     return send_from_directory('src/static', 'index.html')
@@ -17,12 +16,19 @@ def correct_photo():
 
     if content:
         if content.get('photo') and content.get('photo') != '':
+            photos = content.get('photo')
+
+            if type(photos) == str:
+                photos = [photos]
+
             # CNN
-            pass
+
+            # TODO: return CNN output pictures
+            return None
         else: # error, return error code 400
-            return json.dumps('{photo: "", code: 400}')
+            return json.dumps('{photo: [], code: 400}')
     else:
-        return json.dumps('{photo: "", code: 400}')
+        return json.dumps('{photo: [], code: 400}')
 
 app.run(host="0.0.0.0", port=8080, debug=True)
 
