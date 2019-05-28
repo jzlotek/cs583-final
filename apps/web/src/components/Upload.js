7549cd0655ef0cecadd2dc8b64abd1f573752396
Joseph Hines <jthines3@gmail.com>
2019-05-28 18:51:28 -0400

    .

--- apps/web/src/components/Upload.js
+++ apps/web/src/components/Upload.js
@@ -6,11 +6,38 @@ class Upload extends React.Component {
   constructor(props) {
     super(props)
     this.state = {}
+    this.fileInputRef = React.createRef()
+
+    this.openFileDialog = this.openFileDialog.bind(this)
+    this.onFilesAdded = this.onFilesAdded.bind(this)
+  }
+
+  onFilesAdded(evt) {
+    if (this.props.disabled) return
+    const files = evt.target.files
+    // pass files to function to make POST
+    console.log(files)
+  }
+
+  openFileDialog() {
+    if (this.props.disabled) return
+    this.fileInputRef.current.click()
   }
 
   render() {
     return (
-      <div className="Upload">
+      <div
+        className="Upload"
+        onClick={this.openFileDialog}
+        style={{ cursor: this.props.disabled ? 'default' : 'pointer' }}
+      >
+        <input
+          ref={this.fileInputRef}
+          className="FileInput"
+          type="file"
+          multiple
+          onChange={this.onFilesAdded}
+        />
         <span className="Title">Upload Files</span>
       </div>
     )
