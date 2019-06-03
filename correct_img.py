5d48dfcb59d9f2ff27501cd74db0fffdce8eba09
John Zlotek <jzlotek@gmail.com>
2019-06-02 23:30:10 -0400

    CLI app

---
+++ correct_img.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import sys
+import os
+import imageio
+import src.net as net
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Must provide arguments {raw}')
+        exit(1)
+
+    raw = sys.argv[1]
+    dest = sys.argv[2] if len(sys.argv) == 3 else './'
+
+    if not os.path.isfile(raw):
+        print(f'{raw} is not a file')
+        exit(1)
+
+    processed_img = net.net(raw)
+
+    imageio.imwrite(f'{dest}/{raw.split(".")[0]}.png', processed_img)
+    print(f'image created at {dest}/{raw.split(".")[0]}.png')
